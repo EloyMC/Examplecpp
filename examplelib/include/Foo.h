be170fde2e3cae699f3e70becb1565c5414f9a7e
Heiko van der Heijden <heikovanderheijden@Heikos-MacBook-Pro.local>
2018-01-22 12:34:44 +0100

    Initial commit

---
+++ examplelib/include/Foo.h
@@ -0,0 +1,8 @@
+#ifndef EXAMPLELIB_FOO_H
+#define EXAMPLELIB_FOO_H
+#include <string>
+namespace Foo {
+    std::string bar(const std::string& hello);
+}; // Foo
+
+#endif // EXAMPLELIB_FOO_H
---
+++ examplelib/src/Foo.cxx
@@ -0,0 +1,6 @@
+#include "Foo.h"
+
+
+std::string Foo::bar(const std::string& hello) {
+    return hello + " world";
+}
